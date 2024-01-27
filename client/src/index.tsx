c17d84c5e5bff43ab645dd18e8cb674b6dc1a7cc
OlehDutchenko <dutchenko.o.dev@gmail.com>
2024-01-27 23:22:27 +0200

    Adds the ApolloClient to the client's React app

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,20 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import GlobalStyles from './styles';
 import Pages from './pages';
+import GlobalStyles from './styles';
+
+const client = new ApolloClient({
+	uri: 'http://localhost:4000',
+	cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<ApolloProvider client={client}>
+			<GlobalStyles/>
+			<Pages/>
+		</ApolloProvider>,
+	</React.StrictMode>,
+	document.getElementById('root')
 );
