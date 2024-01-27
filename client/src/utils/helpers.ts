5ffc6b7eb3dd19a4284ff78024e579bdfed2b3db
OlehDutchenko <dutchenko.o.dev@gmail.com>
2024-01-27 23:23:01 +0200

    Updates the code style

--- client/src/utils/helpers.ts
+++ client/src/utils/helpers.ts
@@ -3,17 +3,17 @@
  * s, m or H:m (not m:s or H:m:s)
  */
 export const humanReadableTimeFromSeconds = (seconds: number): string => {
-  if (seconds < 60) {
-    return `${seconds}s`;
-  }
-  const totalMinutes = Math.floor(seconds / 60);
-  let hours = Math.floor(totalMinutes / 60) || 0;
-  const minutestoDisplay = totalMinutes % 60;
-  let timeStr = ``;
-  if (hours > 0) {
-    timeStr += `${hours}h `;
-  }
-  timeStr += `${minutestoDisplay}m`;
+	if (seconds < 60) {
+		return `${seconds}s`;
+	}
+	const totalMinutes = Math.floor(seconds / 60);
+	let hours = Math.floor(totalMinutes / 60) || 0;
+	const minutestoDisplay = totalMinutes % 60;
+	let timeStr = ``;
+	if (hours > 0) {
+		timeStr += `${hours}h `;
+	}
+	timeStr += `${minutestoDisplay}m`;
 
-  return timeStr;
+	return timeStr;
 };
