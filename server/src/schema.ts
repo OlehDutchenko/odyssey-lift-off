a372d307eabd688e706d87ff31002ceb2ea91daa
OlehDutchenko <dutchenko.o.dev@gmail.com>
2024-02-11 16:56:52 +0200

    Updates `schema.ts` with new the query endpoint `module(id: ID!)`

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -4,9 +4,12 @@ export const typeDefs = gql`
 	type Query {
 		"Get tracks array for homepage grid"
 		tracksForHome: [Track!]!
-		
+
 		"Get a specific track Track by ID"
 		track(id: ID!): Track
+
+		"Get a specific module Module by ID"
+		module(id: ID!): Module
 	}
 
 	"A track is a group of Modules that teaches about a specific topic"
@@ -22,11 +25,11 @@ export const typeDefs = gql`
 		length: Int
 		"The number of modules this track contains"
 		modulesCount: Int
-        "The track's complete description, can be in Markdown format"
-        description: String
-        "The number of times a track has been viewed"
-        numberOfViews: Int
-        "The track's complete array of Modules"
+		"The track's complete description, can be in Markdown format"
+		description: String
+		"The number of times a track has been viewed"
+		numberOfViews: Int
+		"The track's complete array of Modules"
 		modules: [Module!]!
 	}
 
@@ -39,12 +42,16 @@ export const typeDefs = gql`
 		photo: String
 	}
 
-    "A Module is a single unit of teaching. Multiple Modules compose a Track"
-    type Module {
-        id: ID!
-        "The Module's title"
-        title: String!
-        "The Module's length in minutes"
-        length: Int
-    }
+	"A Module is a single unit of teaching. Multiple Modules compose a Track"
+	type Module {
+		id: ID!
+		"The Module's title"
+		title: String!
+		"The Module's length in minutes"
+		length: Int
+		"The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
+		content: String
+		"The module's video url, for video-based modules"
+		videoUrl: String
+	}
 `;
