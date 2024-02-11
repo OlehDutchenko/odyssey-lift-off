b0aed30bed6a1a154df98f3bb042ee6d513d519d
OlehDutchenko <dutchenko.o.dev@gmail.com>
2024-02-11 11:47:06 +0200

    Adds the Track page

---
+++ client/src/pages/track.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { gql } from '../__generated__';
+import { useQuery } from '@apollo/client';
+import { Layout, QueryResult } from '../components';
+import { useParams } from 'react-router-dom';
+
+
+
+const GET_TRACK = gql(`query GetTrack($trackId: ID!) {
+  track(id: $trackId) {
+    id
+    title
+    author {
+      id
+      name
+      photo
+    }
+    thumbnail
+    length
+    modulesCount
+    numberOfViews
+    modules {
+      id
+      title
+      length
+    }
+    description
+  }
+}`)
+
+const Track: React.FC = () => {
+	const { trackId = '' } = useParams();
+	console.log(trackId)
+	return <Layout />
+}
+
+export default Track;
